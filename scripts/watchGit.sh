0b42c97e3d04a8dc516225d2781e6f2112f2688e
Jan Mrázek <email@honzamrazek.cz>
2020-05-14 18:46:56 +0200

    Add test server run script

---
+++ scripts/watchGit.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+. .env/bin/activate
+
+while :
+do
+    date
+    git pull
+    pip install -r requirements.txt
+    python manage.py tailwind install
+    python manage.py tailwind build
+    python manage.py collectstatic --noinput
+    scripts/resetGame.sh
+    sudo systemctl restart gunicorn
+
+    while :
+    do
+        sleep 61
+        git fetch
+        if git status | grep 'behind'
+        then
+            break
+        fi
+    done
+done
