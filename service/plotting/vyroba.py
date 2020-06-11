56f1e1346f9779fd2b22c20484be2dfd118a0c93
Jan Mrázek <email@honzamrazek.cz>
2020-06-11 10:58:12 +0200

    Update vyroba plotting

--- service/plotting/vyroba.py
+++
@@ -1,2 +0,0 @@
-from service.plotting.dot import indent, escapeId, fromMm, digraphHeader, endGraph
-
--- service/plotting/vyroby.py
+++ service/plotting/vyroby.py
@@ -29,55 +29,13 @@ class VyrobaBuilder:
             sys.stderr.write(e.stderr.decode("utf8"))
             raise
 
-    def generateTechLabels(self):
-        for tech in TechModel.objects.all():
-            self.generateTechLabel(tech)
+    def generateVyrobaLabels(self):
+        for vyroba in VyrobaModel.objects.all():
+            self.generateVyrobaLabel(vyroba)
 
-    def labelFor(self, tech):
-        """Get absolute file path for given tech label"""
-        return os.path.join(self.buildDirectory, tech.id + ".pdf")
-
-    def emptyLabelFor(self, tech):
-        """Get absolute file path for given tech label"""
-        return os.path.join(self.buildDirectory, "empty-" + tech.id + ".pdf")
-
-    def fullGraphFile(self):
-        return os.path.join(self.buildDirectory, "fullGraph.pdf")
-
-    def emptyFullGraphFile(self):
-        return os.path.join(self.buildDirectory, "emptyFullGraph.pdf")
-
-    def generateFullGraph(self):
-        return self.generateFullGraphImpl(self.fullGraphFile(), lambda x: self.labelFor(x))
-
-    def generateEmptyFullGraph(self):
-        return self.generateFullGraphImpl(self.emptyFullGraphFile(), lambda x: self.emptyLabelFor(x))
-
-    def generateFullGraphImpl(self, fullGraphFile, techLabler):
-        try:
-            dotFile = os.path.join(self.buildDirectory, fullGraphFile.replace(".pdf", ".dot"))
-            texFile = os.path.join(self.buildDirectory, fullGraphFile.replace(".pdf", ".tex"))
-            techs = TechModel.objects.all()
-            with open(dotFile, "w") as f:
-                digraphHeader(f)
-                for t in techs:
-                    declareTech(f, t, techLabler(t))
-                for t in techs:
-                    declareTechEdges(f, t)
-                endGraph(f)
-            buildCmd = ["dot2tex", "--autosize", "--usepdflatex", "-ftikz",
-                "--template", "service/plotting/graphTemplate.tex",
-                "-o", texFile, dotFile]
-            subprocess.run(buildCmd, capture_output=True, check=True)
-            buildCmd = ["texfot", "pdflatex", "-halt-on-error",
-                    "--output-directory", self.buildDirectory, texFile]
-            subprocess.run(buildCmd, capture_output=True, check=True)
-        except subprocess.CalledProcessError as e:
-            cmd = " ".join(e.cmd)
-            sys.stderr.write(f"Command '{cmd}' failed:\n")
-            sys.stderr.write(e.stdout.decode("utf8"))
-            sys.stderr.write(e.stderr.decode("utf8"))
-            raise
+    def labelFor(self, vyroba):
+        """Get absolute file path for given vyroba label"""
+        return os.path.join(self.buildDirectory, vyroba.id + ".pdf")
 
     def formatResource(self, resource):
         return resource.label
@@ -105,11 +63,38 @@ class VyrobaBuilder:
         \newcommand\VyrobaCard[4]{%
             \setlength\fboxsep{0.3cm}\setlength\fboxrule{0.0pt}% delete
             \fbox{% delete
-                    \begin{minipage}[c][5cm][t]{9cm}%
+                    \begin{minipage}[c][3.2cm][t]{9cm}%
                         \begin{tabularx}{\textwidth}{lXr}
                             \raisebox{-\height+\fontcharht\font`X}{#1} \vspace{0.2cm} & #2 & \raisebox{-\height+\fontcharht\font`X}{{#3}}
                         \end{tabularx}
                         {#4}
+                    \end{minipage}%
+            }% delete
+        }
+        """
+
+    def enhancementHeader(self):
+        return r"""
+        \documentclass{standalone}
+        \usepackage[czech]{babel}
+        \usepackage[utf8]{inputenc}
+        \usepackage[T1]{fontenc}
+        \usepackage{tabularx}
+        \usepackage{amsmath}
+        \usepackage{txfonts}
+        \usepackage{mdframed}
+        \usepackage{qrcode}
+        \usepackage{pbox}
+        \usepackage{enumitem}
+        \usepackage{graphicx}
+
+        \renewcommand*{\arraystretch}{0}
+
+        \newcommand\EnhancementCard[1]{%
+            \setlength\fboxsep{0.3cm}\setlength\fboxrule{0.0pt}% delete
+            \fbox{% delete
+                    \begin{minipage}[c][3cm][t]{3cm}%
+                        #1
                     \end{minipage}%
             }% delete
         }
@@ -120,52 +105,36 @@ class VyrobaBuilder:
         Generate LaTeX file with tech node
         """
 
-        description = r"{\Large\textbf{" + tech.label + r"}}" + "\n\n"
-        description += tech.flavour + "\n\n"
-        vyrobas = tech.unlock_vyrobas.all()
-        if vyrobas and True:
-            if len(vyrobas) == 1:
-                description += r"\textbf{Odemyká výrobu:}"
-            else:
-                description += r"\textbf{Odemyká výroby:}"
-            description += r"\begin{itemize}[noitemsep,nolistsep,leftmargin=*,after=\vspace*{-\dimexpr\baselineskip - 3pt}]" + "\n"
-            for vyroba in vyrobas:
-                description += r"\item " + vyroba.label + "\n"
-            description += r"\end{itemize}" + "\n\n"
-        enhancers = tech.unlock_enhancers.all()
-        if enhancers:
-            description += r"\textbf{Odemyká vylepšení:}"
-            description += r"\begin{itemize}[noitemsep,nolistsep,leftmargin=*,after=\vspace*{-\dimexpr\baselineskip - 3pt}]" + "\n"
-            for enhancer in enhancers:
-                description += r"\item " + enhancer.label + "\n"
-            description += r"\end{itemize}" + "\n\n"
+        description = r"{\Large\textbf{" + vyroba.label + r"}}" + "\n\n"
+        description += vyroba.flavour + "\n\n"
+        description += r"\textbf{Probíhá v: }" + vyroba.build.label + "\n\n"
 
-        unlocksTech = tech.unlocks_tech.all()
-        unlocks = ""
-        if unlocksTech:
-            unlocks += r"\textbf{Navazující směry bádání:}\begin{itemize}[noitemsep,nolistsep,leftmargin=*]" + "\n"
-            for ut in unlocksTech:
-                resources = ["{}$\\times$ {}".format(ut.dots, ut.die.label)]
-                resources += ["{}$\\times$ {}".format(r.amount, self.formatResource(r.resource)) for r in ut.resources.all()]
-                unlocks += r"\item " + ut.label + " (" + ", ".join(resources) + ")\n"
-            unlocks += r"\end{itemize}" + "\n\n"
+        longDescription = ""
+        longDescription += r"\textbf{Výstup: }" + f"{vyroba.amount} $\\times$ {self.formatResource(vyroba.output)} \n\n"
+        longDescription += r"\textbf{Vstupy: }"
+        resources = ["{}$\\times$ {}".format(vyroba.dots, vyroba.die.label)]
+        resources += ["{}$\\times$ {}".format(r.amount, self.formatResource(r.resource)) for r in vyroba.inputs.all()]
+        longDescription += ", ".join(resources) + "\n\n"
 
-        if tech.image and tech.image != "-":
-            icon = r"\includegraphics[width=2.5cm, height=2.5cm, keepaspectratio]{" + os.path.join(self.iconDirectory, tech.image) + r"}"
+        description += longDescription
+
+        if vyroba.output.icon and vyroba.output.icon != "-":
+            icon = r"\includegraphics[width=1.5cm, height=1.5cm, keepaspectratio]{" + os.path.join(self.iconDirectory, vyroba.output.icon) + r"}"
         else:
             icon = ""
 
-        file.write(self.cardHeader(self.teamColor))
+        file.write(self.vyrobaHeader())
         file.write(r"""
         \begin{document}
             \VyrobaCard
-                {\qrcode[version=1,height=2cm]{""" + tech.id + r"""}}
+                {\qrcode[version=1,height=2cm]{""" + vyroba.id + r"""}}
                 {""" + description + r"""}
                 {""" + icon + r"""}
-                {""" + unlocks + r"""
-                    \vspace*{\fill}
-                    \begin{center} (""" + tech.nodeTag + r""")\end{center}
-                }
-
+                {""" + "" + r"""}
         \end{document})
         """)
+
+    def enhancementCard(self, file, enh):
+        description = r"{\Large\textbf{" + enh.label + r"}}" + "\n\n"
+        description += vyroba.flavour + "\n\n"
+        description += r"\textbf{Probíhá v: }" + vyroba.build.label + "\n\n"
