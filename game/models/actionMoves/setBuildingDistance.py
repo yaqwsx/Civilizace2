55fb293e860a7a81a27950f4cace732d841b265e
Jan Mrázek <email@honzamrazek.cz>
2020-06-17 20:12:07 +0200

    Make building distance more friendly

--- game/models/actionMoves/setBuildingDistance.py
+++ game/models/actionMoves/setBuildingDistance.py
@@ -10,62 +10,27 @@ from game.models.state import MissingDistanceError
 from game.data.tech import TechModel
 
 
-def preFillDistances(source, target, distance, distances):
-    fmtDist = []
-    for b, dist in distances.items():
-        fmtDist.append([b[0].id, b[1].id, dist])
-
-    return r'''
-    <script>
-        var source = document.getElementById("''' + source.id_for_label + r'''");
-        var target = document.getElementById("''' + target.id_for_label + r'''");
-        var distance = document.getElementById("''' + distance.id_for_label + r'''")
-
-        var distances = ''' + json.dumps(fmtDist) + r''';
-        function update() {
-            var src = source.value;
-            var tgt = target.value;
-            if (src == tgt) {
-                distance.value = 0;
-                return;
-            }
-            for (var i = 0; i != distances.length; i++ ) {
-                var item = distances[i];
-                if ( (item[0] == src && item[1] == tgt) || (item[1] == src && item[0] == tgt)) {
-                    distance.value = item[2];
-                    return;
-                }
-                distance.value = "";
-            }
-        }
-
-        source.addEventListener("change", update);
-        target.addEventListener("change", update);
-        update();
-    </script>
-    '''
-
 class SetBuildingDistanceForm(MoveForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        distanceLogger = self.state.teamState(self.teamId).distances
         buildings = self.state.teamState(self.teamId).techs.getBuildings()
-        choices = [(x.id, x.label) for x in buildings]
-        self.fields["source"] = \
-            forms.ChoiceField(choices=choices, label="První budova")
-        self.fields["target"] = \
-            forms.ChoiceField(choices=choices, label="Druhá budova")
-        self.fields["distance"] = forms.IntegerField(min_value=0, label="Vzdálenost")
-
-        distances = self.state.teamState(self.teamId).distances.allBuildingDistances()
-
-        self.helper.layout = Layout(
-            self.commonLayout,
-            "source",
-            "target",
-            "distance",
-            HTML(preFillDistances(self["source"], self["target"],
-                self["distance"], distances))
-        )
+        sourceBuilding = TechModel.objects.get(id=self.entityId)
+        for b in buildings:
+            if b.id == self.entityId:
+                continue
+            try:
+                distance = distanceLogger.getBuildingDistance(sourceBuilding, b)
+                self.fields[b.id] = forms.IntegerField(
+                    label=b.label,
+                    initial=distance,
+                    max_value=distance,
+                    min_value=0)
+            except MissingDistanceError:
+                self.fields[b.id] = forms.IntegerField(
+                    label=b.label,
+                    min_value=0,
+                    required=False)
 
 class SetBuildingDistanceMove(Action):
     class Meta:
@@ -77,7 +42,7 @@ class SetBuildingDistanceMove(Action):
 
     @staticmethod
     def relevantEntities(state, team):
-        return []
+        return state.teamState(team).techs.getBuildings()
 
     def build(data):
         action = SetBuildingDistanceMove(
@@ -89,24 +54,41 @@ class SetBuildingDistanceMove(Action):
     def initiate(self, state):
         return True, ""
 
+    def formatDistanceChange(self, change):
+        if change[0]:
+            return f'{change[0]} &rarr; {change[1]}'
+        return f'Neznámá &rarr; {change[1]}'
+
     def commit(self, state):
         distLogger = state.teamState(self.team.id).distances
-        source = TechModel.objects.get(id=self.arguments["source"])
-        target = TechModel.objects.get(id=self.arguments["target"])
-        distance = self.arguments["distance"]
-        distLabel = f"{source.label}&harr;{target.label}"
-        try:
-            originalDistance = distLogger.getBuildingDistance(source, target)
-            if originalDistance <= distance:
-                message = f"""
-                    Původní vzdálenost pro {distLabel} ({originalDistance}) je menší než nová ({distance}).<br>
-                    Vzdálenost nelze aktualizovat.
-                """
-                return False, message
-            distanceMessage = f"Původní vzdálenost {distLabel}: <b>{originalDistance}</b>"
-        except MissingDistanceError:
-            distanceMessage = f"Původní vzdálenost {distLabel}: <b>neznámá</b>"
-        distLogger.setBuildingDistance(source, target, distance)
-        message = f"""{distanceMessage}<br>
-            Nová vzdálenost: <b>{distance}</b>"""
-        return True, message
+        source = TechModel.objects.get(id=self.arguments["entity"])
+        buildings = state.teamState(self.team.id).techs.getBuildings()
+        messages = []
+        distances = {}
+        for b in buildings:
+            if b.id == source.id:
+                continue
+            if b.id not in self.arguments:
+                return False, f"Chybí vzdálenostní informace pro budovu {b.label}"
+            distance = self.arguments[b.id]
+            if not distance:
+                continue
+            try:
+                originalDistance = distLogger.getBuildingDistance(source, b)
+                if originalDistance < distance:
+                    messages.append(f"""
+                        Původní vzdálenost pro {b.label} ({originalDistance}) je menší než nová ({distance}).<br>
+                        Vzdálenost nelze aktualizovat.
+                    """)
+                else:
+                    distLogger.setBuildingDistance(source, b, distance)
+                    if distance != originalDistance:
+                        distances[b] = (originalDistance, distance)
+            except MissingDistanceError:
+                distLogger.setBuildingDistance(source, b, distance)
+                distances[b] = (None, distance)
+        if messages:
+            errors = "".join([f'<li>{x}</li>' for x in messages])
+            return False, f'Nelze zaktualizovat vzdálenosti: <ul class="list-disc px-4">{errors}</ul>'
+        msg = "".join([f'<li>{source.label}&harr;{b.label}: {self.formatDistanceChange(change)}' for b, change in distances.items()])
+        return True, f'Následující vzdálenosti budou aktualizovány:  <ul class="list-disc px-4">{msg}</ul>'
