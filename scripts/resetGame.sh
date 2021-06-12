a8c34173750504882499d2b3124ae3c456f26dfc
Henrich Lauko <xlauko@mail.muni.cz>
2021-06-12 10:13:48 +0200

    game: Fix reset script.

--- scripts/resetGame.sh
+++ scripts/resetGame.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm /home/xmrazek7/Civilizace/db.sqlite3
+rm db.sqlite3
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py create entities groups users state
