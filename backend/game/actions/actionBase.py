53b38f4a4f7c38046f58e9a8acbe9c737baf0cc6
Jiri Marek <marejir@gmail.com>
2022-04-06 17:58:45 +0200

    Formatted whitespaces

--- backend/game/actions/actionBase.py
+++ backend/game/actions/actionBase.py
@@ -6,18 +6,18 @@ from game.state import GameState, TeamId
 
 
 class ActionBase(BaseModel):
-	teamId: TeamId
-	entities: Entities
-	state: GameState
+    teamId: TeamId
+    entities: Entities
+    state: GameState
 
-	errors: MessageBuilder = MessageBuilder()
-	info: MessageBuilder = MessageBuilder()
+    errors: MessageBuilder = MessageBuilder()
+    info: MessageBuilder = MessageBuilder()
 
-	def cost(self) -> ActionCost:
+    def cost(self) -> ActionCost:
         raise NotImplementedError("ActionBase is an interface")
 
     def commit(self) -> str:
         self.apply()
 
-	def apply(self) -> None:
+    def apply(self) -> None:
         raise NotImplementedError("ActionBae is an interface")
