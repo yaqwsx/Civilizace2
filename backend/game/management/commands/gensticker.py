d2ad5736c12abf4f6bda3f025b07be9562cf122e
Jan Mrázek <email@honzamrazek.cz>
2022-06-17 00:14:58 +0200

    Add team option for script generator

--- backend/game/management/commands/gensticker.py
+++ backend/game/management/commands/gensticker.py
@@ -14,10 +14,13 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("entity", type=str)
         parser.add_argument("type", type=str)
+        parser.add_argument("team", type=str)
         parser.add_argument("output", type=str)
 
-    def handle(self, entity, type, output, *args, **kwargs):
+    def handle(self, entity, type, team, output, *args, **kwargs):
         _, entities = DbEntities.objects.get_revision()
+
+        teamModel = Team.objects.get(pk=team)
 
         stickerType = {
             "regular": StickerType.regular,
@@ -25,7 +28,7 @@ class Command(BaseCommand):
             "techfirst": StickerType.techFirst
         }[type.lower()]
 
-        img = makeSticker(entities[entity], Team.objects.get(pk="tym-zeleni"), stickerType)
+        img = makeSticker(entities[entity], teamModel, stickerType)
         img.save(output)
         print(f"Height: {int(img.height / 180 * 25.4)}")
 
