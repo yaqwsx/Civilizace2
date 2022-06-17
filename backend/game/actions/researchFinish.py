b061e854154c98a097517abd51f2baff2b837644
Jiri Marek <marejir@gmail.com>
2022-06-18 00:45:44 +0200

    Rewrite notification message

--- backend/game/actions/researchFinish.py
+++ backend/game/actions/researchFinish.py
@@ -4,6 +4,7 @@ from game.actions.actionBase import ActionBase, ActionFailed
 from game.actions.researchStart import ActionResearchArgs
 from game.entities import Resource, Tech, dieName
 
+
 class ActionResearchFinish(ActionResearchArgs):
     pass
 
@@ -23,33 +24,41 @@ class ActionResearchFinish(ActionBase):
 
     def _commitImpl(self) -> None:
         if self.args.tech in self.teamState.techs:
-            raise ActionFailed(f"Technologie [[{self.args.tech.id}]] je již vyzkoumána.")
+            raise ActionFailed(
+                f"Technologie [[{self.args.tech.id}]] je již vyzkoumána.")
 
         if not self.args.tech in self.teamState.researching:
-            raise ActionFailed(f"Výzkum technologie [[{self.args.tech.id}]] aktuálně neprobíhá, takže ji nelze dokončit.")
+            raise ActionFailed(
+                f"Výzkum technologie [[{self.args.tech.id}]] aktuálně neprobíhá, takže ji nelze dokončit.")
 
         self.teamState.researching.remove(self.args.tech)
         self.teamState.techs.add(self.args.tech)
-        self._info += "Výzkum technologie [[" + self.args.tech.id + "]] byl dokončen."
+        self._info += "Výzkum technologie [[" + \
+            self.args.tech.id + "]] byl dokončen."
         self._info += f"Vydejte týmu puntík na kostku"
-        dice = ", ".join([dieName(die) for die in self.teamState.getUnlockingDice(self.args.tech)])
+        dice = ", ".join(
+            [dieName(die) for die in self.teamState.getUnlockingDice(self.args.tech)])
         self._info += f"Vydejte týmu jeden žeton objevu: {dice}"
 
         # check bonuses
         if self.args.tech.bonuses != "":
             for bonus in self.args.tech.bonuses:
                 if bonus == "cheapDie":
-                    discount = len([tech for tech in self.teamState.techs if "cheapDie" in tech.bonuses])
+                    discount = len(
+                        [tech for tech in self.teamState.techs if "cheapDie" in tech.bonuses])
                     self.teamState.throwCost = 10 - discount
                     continue
                 if "obyvatel" in bonus:
                     count = int(bonus[8:])
                     foodCount = ceil(count / 20)
                     self.receiveResources({self.entities.obyvatel: count})
-                    self.teamState.granary[self.entities.basicFoodProduction] = self.teamState.granary.get(self.entities.basicFoodProduction, 0) + foodCount
-                    self.addNotification(self.args.team, f"Dostali jste {count} nových obyvatel. V zásobě vám také přibylo [[{self.entities.basicFoodProduction}|{foodCount}]], kterými se budou živit.")
+                    self.teamState.granary[self.entities.basicFoodProduction] = self.teamState.granary.get(
+                        self.entities.basicFoodProduction, 0) + foodCount
+                    self.addNotification(
+                        self.args.team, f"Dostali jste {count} nových obyvatel. Do centra vám také přibylo [[{self.entities.basicFoodProduction}|{foodCount}]], kterými se budou živit.")
                 if "kultura" in bonus:
                     count = int(bonus[7:])
-                    self.receiveResources({self.entities["res-kultura"]: count})
-                    self.addNotification(self.args.team, f"Vaše kultura vzrostla o {count}. Těšte se na nové obyvatele!")
-                
+                    self.receiveResources(
+                        {self.entities["res-kultura"]: count})
+                    self.addNotification(
+                        self.args.team, f"Vaše kultura vzrostla o {count}. Těšte se na nové obyvatele!")
